cd402634fbe6b29614c30582c88f4c00a469720a
lan.wei@ucdconnect.ie <lan.wei@ucdconnect.ie>
2017-02-28 16:51:55 +0000

    fourth

--- src/ledtester.py
+++ src/ledtester.py
@@ -1,6 +1,6 @@
 from pprint import pprint
-
-f=open('1.txt','r')
+filename=input('enter a file name:')
+f=open(filename,'r')
 count_1=0
 count = 0
 for line in f:
@@ -16,34 +16,61 @@ for line in f:
             x2=int(line[-2])
             y1=int(line[3])
             y2=int(line[-1])
-
+            if x1<0:
+                x1=0
+            if y1<0:
+                y1=0
+            if x2>=size:
+                x2=size-1
+            if y2>=size:
+                y2=size-1 
             for i in range(x1,x2+1):
                 for j in range(y1,y2+1):
                     light[i][j] = True
+            
         elif line[0] == "turn" and line[1] == "off":
             command = line[0]+line[1]
             x1=int(line[2])
             x2=int(line[-2])
             y1=int(line[3])
             y2=int(line[-1])
+            if x1<0:
+                x1=0
+            if y1<0:
+                y1=0
+            if x2>=size:
+                x2=size-1
+            if y2>=size:
+                y2=size-1 
             for i in range(x1,x2+1):
                 for j in range(y1,y2+1):
                     light[i][j] = False
+            
         elif line[0] == "switch":
             command = line[0]
             x1=int(line[1])
             x2=int(line[-2])
             y1=int(line[2])
             y2=int(line[-1])
+            if x1<0:
+                x1=0
+            if y1<0:
+                y1=0
+            if x2>=size:
+                x2=size-1
+            if y2>=size:
+                y2=size-1 
             for i in range(x1,x2+1):
                 for j in range(y1,y2+1):
                     if light[i][j] == True:
                         light[i][j] = False
                     else:
                         light[i][j] = True
+            
         else:
             continue
 lightOn = 0
 for i in range(size):
     lightOn += sum(light[i]) 
 print(lightOn)
+
